458138ad2441086416e017c9bf58b1fd36433d5e
yeon990806 <yeon990806@gmail.com>
2023-12-11 20:24:06 +0900

    feat(home): add content

--- src/components/Home/HomeDefault.tsx
+++ src/components/Home/HomeDefault.tsx
@@ -1,15 +1,45 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const HomeDefault = () => (
   <section className="home-section" id="home">
     <StyledContainer className="container">
       <StyledImg />
-      <StyledTitle>
-        Doyeon Kim
-      </StyledTitle>
-      <StyledSubTitle>
-        Web Front Developer
-      </StyledSubTitle>
+      <StyledInfo>
+          <StyledBar />
+          <h1>
+            Doyeon Kim
+          </h1>
+          <h2>
+            Web Front Developer
+          </h2>
+          <p>
+            I like to improve the user experience and<br />
+            study new technologies that are on the trend.
+          </p>
+        </StyledInfo>
+        <StyledArticleContainer>
+          <article>
+            <h6>ABOUT ME</h6>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br />
+            </p>
+            <Link to="">
+              LEARN MORE
+            </Link>
+          </article>
+          <article>
+            <h6>MY WORK</h6>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br />
+            </p>
+            <Link to="">
+              LEARN MORE
+            </Link>
+          </article>
+        </StyledArticleContainer>
     </StyledContainer>
   </section>
 );
@@ -17,8 +47,18 @@ const HomeDefault = () => (
 const StyledContainer = styled.section`
   position: relative;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  p {
+    margin-top: 16px;
+    font-size: 20px;
+    font-weight: 500;
+    color: #b9b9b9;
+    line-height: 1.25;
+    max-width: 480px;
+  }
 `;
 
 const StyledImg = styled.div`
@@ -28,26 +68,87 @@ const StyledImg = styled.div`
   transform: translateY(-50%);
   width: 40%;
   aspect-ratio: 8/7;
-  background-image: url('/assets/images/home_img.jpg');
+  /*background-image: url('/assets/images/home_img.jpg');*/
   border-radius: 70% 30% 30% 70% / 60% 40% 60% 40%;
   background-position: center center;
   background-size: cover;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
 `;
 
-const StyledTitle = styled.h1`
-  margin-left: 24px;
-  font-size: 48px;
-  font-weight: 700;
-  color: #f5f7fa;
+const StyledBar = styled.div`
+  margin-bottom: 16px;
+  width: 8vw;
+  height: 4px;
+  background-color: #f5f7fa;
+  border-radius: 1px;
 `;
 
-const StyledSubTitle = styled.h2`
-  margin-left: 24px;
-  margin-top: 8px;
-  font-size: 28px;
-  font-weight: 400;
-  color: #e7e7e7;
+const StyledInfo = styled.div`
+  h1 {
+    font-size: 48px;
+    font-weight: 700;
+    color: #f5f7fa;
+  }
+
+  h2 {
+    margin-top: 8px;
+    font-size: 28px;
+    font-weight: 400;
+    color: #e7e7e7;
+  }
+`;
+
+const StyledArticleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
+
+  article {
+    h6 {
+      font-size: 24px;
+      font-weight: 700;
+      color: #f5f7fa;
+    }
+
+    a {
+      position: relative;
+      display: inline-block;
+      padding: 8px 4px;
+      margin-top: 24px;
+      font: inherit;
+      color: #f5f7fa;
+      text-decoration: none;
+      overflow: hidden;
+
+      &:hover::after {
+        animation-duration: 0.8s;
+        animation-timing-function: ease-in;
+        animation-name: anim;
+      }
+
+      &::after {
+        content: '';
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: #f5f7fa;
+      }
+    }
+  }
+
+  @keyframes anim {
+    from {
+      transform: translateX(-100%);
+    }
+
+    to {
+      transform: translateX(0%);
+    }
+  }
 `;
 
 export default HomeDefault;

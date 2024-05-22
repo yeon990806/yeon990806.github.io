3222305ca28d10bee55b6b8e9ac1dc0da1222c59
yeon990806 <yeon990806>
2024-05-22 19:43:22 +0900

    chore: remix.config 추가

---
+++ remix.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  future: {
+    /* any enabled future flags */
+  },
+  ignoredRouteFiles: ["**/*.css"],
+  publicPath: "/",
+  routes(defineRoutes) {
+    return defineRoutes((route) => {
+      route("/somewhere/cool/*", "catchall.tsx");
+    });
+  },
+  serverBuildPath: "build/index.js",
+};

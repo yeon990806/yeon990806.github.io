41885676bd539f7b3890881c9f1d6c1c9d227475
yeon990806 <yeon990806>
2024-05-22 20:13:27 +0900

    chore: testing getPosts

--- app/libs/constants/dataset.ts
+++ app/libs/constants/dataset.ts
@@ -1,15 +1,15 @@
-import { glob } from "glob";
-import path from "path";
+// import { glob } from "glob";
+// import path from "path";
 
-// post 저장 디렉토리
-export const POST_DIRECTORY = path.join(path.resolve(), "../../posts");
+// // post 저장 디렉토리
+// export const POST_DIRECTORY = path.join(path.resolve(), "../../posts");
 
-// post mdx를 가져오는 함수
-export const getPostFiles = () => glob.sync(`${POST_DIRECTORY}/**/*.mdx`);
+// // post mdx를 가져오는 함수
+// export const getPostFiles = () => glob.sync(`${POST_DIRECTORY}/**/*.mdx`);
 
-export const importPost = (filePath: string) => import(filePath).then(mod => ({
-  sug: path.basename(filePath, path.extname(filePath)),
-  ...mod.attributes.meta
-}));
+// export const importPost = (filePath: string) => import(filePath).then(mod => ({
+//   sug: path.basename(filePath, path.extname(filePath)),
+//   ...mod.attributes.meta
+// }));
 
-export const getAllPosts = async () => await Promise.all(getPostFiles().map(importPost));
+// export const getAllPosts = async () => await Promise.all(getPostFiles().map(importPost));
--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,21 +1,21 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { getAllPosts } from "~/libs/constants/dataset";
+// import { getAllPosts } from "~/libs/constants/dataset";
 
-const loader = async () => json(await getAllPosts());
+// const loader = async () => json(await getAllPosts());
 
 const Blog = () => {
-  const postList = useLoaderData<typeof loader>();
+  // const postList = useLoaderData<typeof loader>();
   return (
     <ul>
-      {postList.map((post) => (
+      {/* {postList.map((post) => (
         <li key={post.slug}>
           <Link to={post.slug}>{post.title}</Link>
           {post.description ? (
             <p>{post.description}</p>
           ) : null}
         </li>
-      ))}
+      ))} */}
     </ul>
   );
 };

7c16c8a705a7e55e7d63b03369a41419fb2a5258
yeon990806 <yeon990806>
2024-07-18 01:07:46 +0900

    chore: 빌드 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const assetPrefix = process.env.NODE_ENV === 'production' ? 'https://yeon990806.github.io/' : ''
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix,
+};
 
 export default nextConfig;

08445c23fde83a2f4a62486c5af8e06857d19e4b
yeon990806 <yeon990806>
2024-08-05 00:44:13 +0900

    feat: added google ad

---
+++ src/components/GoogleAd.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
+}
+
+const GoogleAd = () => {
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({})
+  }, [])
+
+  return (
+    <div className="googleAd-container">
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-format="fluid"
+        data-ad-layout-key="-fb+5w+4e-db+86"
+        data-ad-client="ca-pub-3131973401944410"
+        data-ad-slot="5792448771"
+      />
+    </div>
+  );
+};
+export default GoogleAd;

67b12dedb4234f868b08595e59a21728e03e7700
yeon990806 <yeon990806>
2024-08-05 23:38:28 +0900

    feat: 투자일지

--- src/constant/series.ts
+++ src/constant/series.ts
@@ -2,5 +2,7 @@ import { SeriesType } from "@/libs/types";
 
 export const Series: SeriesType[] = [
   { title: '투자공부', desc: '경제, 금융을 공부하고 인사이트를 얻습니다.', img: '/images/economy.webp', slug: 'InvestmentStudy' },
-  { title: '자바스크립트 Deep Dive', desc: '모던 자바스크립트 Deep Dive 책을 읽으며 자바스크립트를 다시 공부합니다.', img: '/images/js.png', slug: 'DeepDiveJS' }
+  { title: '자바스크립트 Deep Dive', desc: '모던 자바스크립트 Deep Dive 책을 읽으며 자바스크립트를 다시 공부합니다.', img: '/images/js.png', slug: 'DeepDiveJS' },
+  { title: '웹소설 리뷰', desc: '웹소설을 리뷰합니다.', img: '/images/web-novel.webp', slug: 'WebNovel' },
+  { title: '투자일지', desc: '투자 생활을 이어나가며 기억해야 할 날을 기록합니다.', img: '/images/invest-report.webp', slug: 'InvestmentJournal' }
 ];

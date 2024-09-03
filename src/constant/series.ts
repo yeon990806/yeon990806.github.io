0b2c701a47908701be796c87e6109b8058967048
yeon990806 <yeon990806>
2024-09-03 19:09:06 +0900

    feature: 20240903 시리즈 추가

--- src/constant/series.ts
+++ src/constant/series.ts
@@ -4,5 +4,6 @@ export const Series: SeriesType[] = [
   { title: '투자공부', desc: '경제, 금융을 공부하고 인사이트를 얻습니다.', img: '/images/economy.webp', slug: 'InvestmentStudy' },
   // { title: '웹소설 리뷰', desc: '웹소설을 리뷰합니다.', img: '/images/web-novel.webp', slug: 'WebNovel' },
   { title: '투자일지', desc: '투자 생활을 이어나가며 기억해야 할 날을 기록합니다.', img: '/images/invest-report.webp', slug: 'InvestmentJournal' },
-  { title: '바매니저 프로젝트', desc: 'Bar manager 프로젝트를 진행하며 수기를 기록합니다.', img: '/images/bar-manager.webp', slug: 'BarManager' }
+  { title: '바매니저 프로젝트', desc: 'Bar manager 프로젝트를 진행하며 수기를 기록합니다.', img: '/images/bar-manager.webp', slug: 'BarManager' },
+  { title: '관리회계', desc: '24년 가을학기 관리회계 수업 내용을 정리합니다.', img: '/images/management-accounting.webp', slug: 'ManagementAccounting' }
 ];
